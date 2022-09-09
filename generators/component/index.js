595f7634b5c6ddf0d9a27dd81c64d0035afe7705
M Miller <melissa.miller@pantheon.io>
2022-09-09 14:26:47 -0700

    Update index.js
    Begin generator and add case conversion functions.

--- generators/component/index.js
+++ generators/component/index.js
@@ -1,15 +1,43 @@
 const Generator = require('yeoman-generator');
 
+// Name conversion functions.
+const toTitleCase = (str) => {
+  return str.replace(/\w\S*/g, (m) => m.charAt(0).toUpperCase() + m.substr(1).toLowerCase());
+};
+
+const toKebabCase = (str) => {
+  return str.toLowerCase().replace(/\s/g, '-');
+};
+
+const toPascalCase = (str) => {
+  return str.replace(/\w\S*/g, (m) => m.charAt(0).toUpperCase() + m.substr(1).toLowerCase()).replace(/\s/g, '');
+};
+
+// Start generator.
 module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts);
   }
 
-  method1() {
-    this.log('method 1 just ran');
-  }
+  async prompting() {
+    const answers = await this.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Name of component:',
+        validate: (input) => {
+          if (!input.match(/^[a-zA-Z0-9 ]+$/)) {
+            return 'Invalid tag name given. Accepts alphanumeric characters and spaces only.';
+          }
+          return true;
+        },
+      },
+    ]);
 
-  method2() {
-    this.log('method 2 just ran');
+    let componentLabel = toTitleCase(answers.name);
+    let componentKebabName = toKebabCase(answers.name);
+    let componentPascalName = toPascalCase(answers.name);
+
+    this.log('Component Name', componentLabel, 'has been created.');
   }
 };
