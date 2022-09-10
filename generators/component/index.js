6e61329ef46c53388d6773cc8a1e7c6760c8de77
M Miller <melissa.miller@pantheon.io>
2022-09-09 20:11:06 -0700

    Update index.js
    Add precheck for directory before continuing

--- generators/component/index.js
+++ generators/component/index.js
@@ -18,58 +18,67 @@ module.exports = class extends Generator {
   }
 
   async prompting() {
-    this.answers = await this.prompt([
+    this.precheck = await this.prompt([
       {
         type: 'confirm',
         name: 'directoryCheck',
-        message: 'Are you in the right directory?',
-      },
-      {
-        type: 'input',
-        name: 'name',
-        message: 'Name of component:',
-        validate: (input) => {
-          if (!input.match(/^[a-zA-Z0-9 ]+$/)) {
-            return 'Invalid tag name given. Accepts alphanumeric characters and spaces only.';
-          }
-          return true;
-        },
-      },
-      {
-        type: 'confirm',
-        name: 'js',
-        message: 'Include a component .js file?',
-        default: false,
+        message: 'Are you running this from the project root?',
       },
     ]);
+
+    if (this.precheck.directoryCheck == false) {
+      this.log('You are not in the right directory, please navigate to the project root and try again.');
+    } else {
+      this.answers = await this.prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'Name of component:',
+          validate: (input) => {
+            if (!input.match(/^[a-zA-Z0-9 ]+$/)) {
+              return 'Invalid tag name given. Accepts alphanumeric characters and spaces only.';
+            }
+            return true;
+          },
+        },
+        {
+          type: 'confirm',
+          name: 'js',
+          message: 'Include a component .js file?',
+          default: false,
+        },
+      ]);
+    }
   }
 
   writing() {
-    const props = {
-      tag: toKebabCase(this.answers.name),
-      component: toPascalCase(this.answers.name),
-      label: toTitleCase(this.answers.name),
-      includeJs: this.answers.js,
-    };
+    if (this.precheck.directoryCheck == true) {
+      const props = {
+        tag: toKebabCase(this.answers.name),
+        component: toPascalCase(this.answers.name),
+        label: toTitleCase(this.answers.name),
+        includeJs: this.answers.js,
+      };
 
-    // File types needed for component.
-    const extensions = ['stories.js', 'docs.mdx', 'twig', 'scss'];
-    if (props.includeJs) {
-      extensions.push('js');
-    }
-
-    // Set destination directory.
-    this.destinationRoot(`${props.tag}`);
-
-    // Generate files for component.
-    extensions.forEach((ext) => {
-      if (ext == 'scss') {
-        this.fs.copyTpl(this.templatePath(`_component.${ext}`), this.destinationPath(`_${props.tag}.${ext}`), props);
-      } else {
-        this.fs.copyTpl(this.templatePath(`component.${ext}`), this.destinationPath(`${props.tag}.${ext}`), props);
+      // File types needed for component.
+      const extensions = ['stories.js', 'docs.mdx', 'twig', 'scss'];
+      if (props.includeJs) {
+        extensions.push('js');
       }
-    });
 
-    this.log(props.label, 'has been created.');
+      // Set destination directory.
+      this.destinationRoot(`${props.tag}`);
+
+      // Generate files for component.
+      extensions.forEach((ext) => {
+        if (ext == 'scss') {
+          this.fs.copyTpl(this.templatePath(`_component.${ext}`), this.destinationPath(`_${props.tag}.${ext}`), props);
+        } else {
+          this.fs.copyTpl(this.templatePath(`component.${ext}`), this.destinationPath(`${props.tag}.${ext}`), props);
+        }
+      });
+
+      this.log(props.label, 'has been created.');
+    }
   }
 };
